fdc30a190f5a48efb7ae166949230454a292cd47
andreedd <ddeeh2@gmail.com>
2020-03-24 19:42:17 +0200

    Project done, i think

--- projekt1/main.R
+++ projekt1/main.R
@@ -1,50 +1,23 @@
 # Title     : Project 1 for the course Forskningsmetodik för IT
-# Objective : evaluate answers from SUS questionnaire
+# Objective : evaluate susData from SUS questionnaire
 
 #loads .csv file into R session
-answers <- read.csv("projekt1/SUSsvar18och20.csv", header = TRUE, sep = ";")
+susData <- read.csv2("projekt1/SUSsvar18och20.csv")
 
-#Makes an empty vector for appending the results
-resultVector <- c()
+#Builds a sequence of even numbers from 2 to column length of csv file
+even <- seq(2, ncol(susData),2)
 
-for (rows in 1:dim(answers)[1]){
-
-  #Pics the rownumber
-  row <- answers[rows,]
-
-  #Factorises the row and uses summary to get how many of which number exsists
-  factRow <- factor(row)
-  sumFactRow <- summary(factRow)
-
-  #Makes values numeric
-  numericFactRow <- as.numeric(levels(factRow))
-
-  #For loop to calculate the SUS values
-  #Selects the number, checks if it is odd or even,
-  #then makes the calculation and multiplies by the amount numbers of a number
-  #Example
-  # 1 2 3 4 Numbers
-  # 1 3 1 5 How many numbers of a number
-  count <- 0
-  total <- 0
-  for (val in sumFactRow){
-    count = count + 1
-    num <- numericFactRow[count]
-    if((num %% 2) == 0) {
-        #EVEN
-        #Substracts the user response from 5 and multiplies by the amount of that number
-        ans <- (5 - num) * sumFactRow[count]
-    } else {
-        #ODD
-        #Substracts 1 from the user response and multiplies by the amount of that number
-        ans <- (num - 1) * sumFactRow[count]
-    }
-    total = total + ans
-  }
-
-  #RESULT
-  result <-  total * 2.5
-  print(result)
-  resultVector <- append(resultVector, result)
+#Loops through the even sequence
+for (evenValue in even){
+  #Even numbers, substracts the response from 5 in all even columns
+  susData[,evenValue] <- 5 - susData[,evenValue]
+  #odd numbers, substracts one from the response in all odd columns
+  susData[,evenValue - 1] <-  susData[,evenValue - 1] - 1
 }
-print(summary(resultVector))
+
+#Adds up all converted responses and converts the range from 0-40 to 0-100
+susResult <- rowSums(susData) * 2.5
+#Prints Min value, Median, Mean and Max
+print(summary(susResult))
+#Prints standard deviation
+cat("Standard deviation: ", sd(susResult))
