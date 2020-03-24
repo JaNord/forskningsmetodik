2ad13d5835799c9e9c514181e2eb2947396d54e8
JaNord <janordma@abo.fi>
2020-03-24 10:13:38 +0200

    added .csv reading

--- projekt1/main.R
+++ projekt1/main.R
@@ -1,4 +1,5 @@
 # Title     : Project 1 for the course Forskningsmetodik för IT
 # Objective : evaluate answers from SUS questionnaire
 
-
+#loads .csv file into R session
+answers <- read.csv("SUSsvar18och20.csv", header = TRUE, sep = ";")
