f2fba37eeee011675fc846f78662e53e6ce4fd09
andreedd <ddeeh2@gmail.com>
2020-03-25 12:24:16 +0200

    removed for loop

--- projekt1/main.R
+++ projekt1/main.R
@@ -7,13 +7,10 @@ susData <- read.csv2("projekt1/SUSsvar18och20.csv")
 #Builds a sequence of even numbers from 2 to column length of csv file
 even <- seq(2, ncol(susData),2)
 
-#Loops through the even sequence
-for (evenValue in even){
-  #Even numbers, substracts the response from 5 in all even columns
-  susData[,evenValue] <- 5 - susData[,evenValue]
-  #odd numbers, substracts one from the response in all odd columns
-  susData[,evenValue - 1] <-  susData[,evenValue - 1] - 1
-}
+#Even numbers, substracts the response from 5 in all even columns
+susData[,even] <- 5 - susData[,even]
+#odd numbers, substracts one from the response in all odd columns
+susData[,even - 1] <-  susData[,even - 1] - 1
 
 #Adds up all converted responses and converts the range from 0-40 to 0-100
 susResult <- rowSums(susData) * 2.5
